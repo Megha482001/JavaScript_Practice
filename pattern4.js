2a00e6269c25f5a79e8b24e39fd179ae07c80abc
megha sarkar <megha_work@LAPTOP-N605D263.localdomain>
2026-06-19 13:39:06 +0530

    four

---
+++ pattern4.js
@@ -0,0 +1,8 @@
+function pattern(num){
+    for (let i = 1; i <= num; i++){
+        row = "";
+        for(let j = 1;j <= i; j++){
+            row += i;
+        } console.log(row);
+    }
+} pattern(4);
