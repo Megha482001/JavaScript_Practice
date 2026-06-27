bbe5a4f91bce304f5964feebbd6bfd2395cacb21
Megha482001 <sarkarmegha2020@gmail.com>
2026-06-27 12:46:19 +0530

    GCD

---
+++ GCD.js
@@ -0,0 +1,9 @@
+function findGcd(num1, num2){
+    let gcd = 1;
+    for (let i = 1; i < Math.min(num1,num2); i++) {
+        if(num1 % i === 0 && num2 % i === 0) {
+            gcd = i;
+        }
+    }
+    console.log(gcd);
+} findGcd(12,8);
